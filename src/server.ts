6331ae9cbe8b9d81bfeb7da0783cd42896c1c99d
Razaul Karim <razaulkarim2410@gmail.com>
2025-12-21 00:26:41 +0600

    create table and add user data

--- src/server.ts
+++ src/server.ts
@@ -48,8 +48,27 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World! Its Me')
 })
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+app.post("/users", async(req: Request, res: Response) => {
+  
+const {name, email} = req.body;
+try{
+const result = await pool.query(`INSERT INTO users(name,email) VALUES($1, $2) RETURNING *`,[name , email] 
+
+);
+// console.log(result.rows[0]);
+res.status(201).json({
+    success: false,
+    message: "Data Inserted Successfully",
+    data: result.rows[0],
+  })
+
+}catch(err: any){
+  res.status(500).json({
+    success: false,
+    message: err.message
+  })
+}
+
   res.status(201).json({
     success:true,
     message: "API is working",
