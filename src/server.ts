af20ae290f6bbbd3c511c30014658e488aab3e19
Razaul Karim <razaulkarim2410@gmail.com>
2025-12-22 12:27:11 +0600

    learn getting all users and single user using params

--- src/server.ts
+++ src/server.ts
@@ -62,18 +62,63 @@ res.status(201).json({
     data: result.rows[0],
   })
 
-}catch(err: any){
+}
+catch(err: any){
   res.status(500).json({
     success: false,
     message: err.message
   })
 }
+})
 
-  res.status(201).json({
-    success:true,
-    message: "API is working",
+app.get("/users", async(req: Request, res: Response)=>{
+  try{
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "User retrieved successfully",
+      data: result.rows,
+
+    })
+
+  }catch(err:any){
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err
+    })
+  }
 })
+
+app.get("/users/:id", async(req: Request, res: Response)=>{
+  // console.log(req.params.id);
+  // res.send({message: "API is cool..."})
+
+  try{  
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+    console.log(result.rows)
+    if(result.rows.length=== 0){
+      res.status(404).json({
+        success: false,
+      message: "User not found",
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: "User fetch successfully",
+        data: result.rows[0],
+      })
+    }
+  }
+  catch(err:any){
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      
+    })
+  }
 })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
